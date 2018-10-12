5c0c1f51114e1b8db4c8b82be37024eae0f04b4f
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-10-12 17:24:39 -0400

    first commit. Setup express and database

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+// Delete Posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect('mongodb://kenny456:kelly456@ds131313.mlab.com:31313/vue-kv-1', {
+        useNewUrlParser: true
+    });
+
+    return client.db('vue-kv-1').collection('posts');
+}
+
+module.exports = router;
